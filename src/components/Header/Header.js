4857a9edbeba8b6ffd7057d003d0f68cd67eba24
Mayowa Falade <falademayowa240@gmail.com>
2018-07-06 09:49:42 +0100

    Update logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Logo from '../../assets/images/logo.jpg';
+import Logo from '../../assets/images/logo.png';
 
 const Header = () => {
   return (
     <div id="header" className="header">
-      <h3>
-        <span>The </span><img src={Logo} alt="App Logo" /><span> Weeks Course</span>
-      </h3>
+      <img src={Logo} alt="App Logo" />
     </div>
   );
 };
